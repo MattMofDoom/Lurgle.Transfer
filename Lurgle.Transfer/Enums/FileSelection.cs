f55eaa359d3f3ebd424523fa99614073deb13b6b
MattMofDoom <matt.marlor@outlook.com>
2022-01-17 11:20:46 +1100

    Add FileSelection enum for clarity

---
+++ Lurgle.Transfer/Enums/FileSelection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lurgle.Transfer.Enums
+{
+    /// <summary>
+    /// Select which filename to use from TransferInfo
+    /// </summary>
+    public enum FileSelection
+    {
+        /// <summary>
+        /// Use TransferInfo.FileName
+        /// </summary>
+        UseFileName,
+        /// <summary>
+        /// Use TransferInfo.SourceFileName
+        /// </summary>
+        UseSourceFileName
+    }
+}
